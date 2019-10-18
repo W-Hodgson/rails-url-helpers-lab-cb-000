63469d6b90ba5d3108858266d2f0a880c6e85466
Will Hodgson <will.hodgson@current-rms.com>
2019-10-18 15:14:14 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
     set_student
   end
 
-  def activate_student
+  def activate
     @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
